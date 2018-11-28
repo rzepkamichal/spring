0480fa8820b3b6f30fdc0b2930115743f0ecd807
rzepkamichal <michrzep@interia.pl>
2018-11-28 08:45:46 +0100

    Spring Container created

--- spring-demo-one/src/com/luv2code/spring/demo/MyApp.java
+++ spring-demo-one/src/com/luv2code/spring/demo/MyApp.java
@@ -5,7 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		
 		//create the object
-		Coach theCoach = new BaseballCoach();
+		Coach theCoach = new TrackCoach();
 		
 		//use tge object
 		System.out.println(theCoach.getDailyWorkout());
