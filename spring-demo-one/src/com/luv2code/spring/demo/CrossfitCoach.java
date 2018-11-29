bd5287874d20ce0de3cb0637ace374c68c86d849
rzepkamichal <michrzep@interia.pl>
2018-11-29 07:27:53 +0100

    Loading context from xml, loading bean from context - excersise ok

---
+++ spring-demo-one/src/com/luv2code/spring/demo/CrossfitCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.spring.demo;
+
+public class CrossfitCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Practice some Kettle Swing!";
+	}
+
+}
--- spring-demo-one/src/com/luv2code/spring/demo/MyApp.java
+++ spring-demo-one/src/com/luv2code/spring/demo/MyApp.java
@@ -5,7 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		
 		//create the object
-		Coach theCoach = new TrackCoach();
+		Coach theCoach = new CrossfitCoach();
 		
 		//use tge object
 		System.out.println(theCoach.getDailyWorkout());
